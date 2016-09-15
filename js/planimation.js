b2104aafcfd4a86f976154326e114c52d2c48fd5
Naser Soueid <naser.soueid@gmail.com>
2016-09-16 00:59:41 +1000

    Added input form generation for global options and types
    I'll need to extend this to cover all input variables and build a way to
    export form input to JSON and restore it based on matching field
    identifiers (with sparse population in case the specification changes in
    between, to save users time when they change the input model).

--- js/planimation.js
+++ js/planimation.js
@@ -5,42 +5,45 @@ var plan_file;
 var readDomain = false;
 var readProblem = false;
 var readPlan = false;
+
+
+var globalOptions = new GlobalOptions();
+var typeOptions = new TypeOptions();
+var animatedObjects;
+var animatedPredicates;
 /*
 ****************      LOAD TEST FILES     **********************
 */
 
 $(document).ready(function(){
-
     $('#inputdomain').on('change', function(e){
         domain_file=this.files[0];
-        try {
-          readFile(this.files[0], function(e) {
-            //manipulate with result...
-            // $('#domain').text(e.target.result);
-          });
-        } catch (x) { alert(x);}
+        // try {
+        //   readFile(this.files[0], function(e) {
+        //     //manipulate with result...
+        //     // $('#domain').text(e.target.result);
+        //   });
+        // } catch (x) { alert(x);}
     });
 
     $('#inputproblem').on('change', function(e){
         problem_file=this.files[0];
-        try {
-          readFile(this.files[0], function(e) {
-            //manipulate with result...
-            // $('#problem').text(e.target.result);
-          });
-        } catch (x) { alert(x); }
+        // try {
+        //   readFile(this.files[0], function(e) {
+        //     //manipulate with result...
+        //     // $('#problem').text(e.target.result);
+        //   });
+        // } catch (x) { alert(x); }
       });
 
     $('#inputplan').on('change', function(e){
         plan_file=this.files[0];
-        try {
-          readFile(this.files[0], function(e) {
-              //manipulate with result...
-              // $('#plan').text(e.target.result);
-          });
-        } catch (x) { alert(x);}
+        // try {
+        //   readFile(this.files[0], function(e) {
+        //       // $('#plan').text(e.target.result);
+        //   });
+        // } catch (x) { alert(x);}
     });
-
 });
 
 function readFile(file, callback){
@@ -53,27 +56,7 @@ function readFile(file, callback){
 ****************      PARSE LOADED FILES     **********************
 */
 
-// TODO: Fix problem with ASYNC FileReader Calls by nesting functions below
-// in callbacks, currently the code below the parsing functions
-// is executed before the parser has finished its job since calls to readFile
-// are not blocking. Solution = Callbacks.
-
-// I can use currying to pass arguments down through the callbacks.
-// ty google: https://www.sitepoint.com/currying-in-functional-javascript/
-
-/*Parses the loaded domain file and returns
-[types, constants, predicates, actionList]*/
-
-
-var domain;
-// [types, constants, predicates, actionList]
-var problem;
-// [objects, startPredicates]
-var plan;
-// [actions]
-
-
-/*Prases the loaded plan and returns a list of actions*/
+/*Parses the loaded plan and returns a list of actions*/
 function parseSolution(domain, problem, callback) {
     readFile(plan_file, function(e) {
       try {
@@ -88,11 +71,9 @@ function parseSolution(domain, problem, callback) {
 
 /*Parses the loaded problem file and returns
 [objects, startPredicates]
-
 NOTE: Sometimes has problems if the file ends in an RPAREN,
 I think the parser misses the EOF token when this is the case, adding a
 whitespace character at the end seems to fix it.*/
-
 function parseProblem(domain, callback) {
       readFile(problem_file, function(e) {
         try {
@@ -105,6 +86,8 @@ function parseProblem(domain, callback) {
       });
 }
 
+/*Parses the loaded domain file and returns
+[types, constants, predicates, actionList]*/
 function parseDomain(callback) {
   readFile(domain_file, function(e) {
     try {
@@ -118,28 +101,122 @@ function parseDomain(callback) {
 }
 
 function parseInputFiles() {
-  // domain;
-  // [[types], [constants], [predicates], [actionList]]
-
-  // problem;
-  // [[objects], [startPredicates]]
-
-  // plan;
-  // [actions]
+  /*
+    domain  = [[types], [constants], [predicates], [actionList]]
+    problem = [[objects], [startPredicates]]
+    plan    = [actions]
+  */
   function getInput(domain,problem,plan) {
-    console.log([domain,problem,plan]);
-    console.log("so far so good");
+    populateAnimationObjects(domain,problem,plan);
     generateInputForm(domain);
-
+    $("#submitInputs").append("<p></p><input id=\"submitInputs\" type=\"button\" "
+          + "value=\"Submit Input\" onclick=\"createAnimationObjects();\">");
   }
     parseDomain(getInput);
 }
 
+/*
+****************      GENERATE INPUT FORM    **********************
+*/
+function generateInputForm(domain) {
+  var spatialOptions
+            = "<select name=\"spatialLayout\"><option value=\"free\">Free</option>"
+            + "<option value=\"network\">Network</option>"
+            + "<option value=\"grid\">Grid</option>"
+            ;
+
+  var globalOptionsInput
+            = "<p>Global Options</p><table><tr>"
+            + "<th>Spatial Layout</th>"
+            + "</tr>"
+            + "<tr><td>"+ spatialOptions +"</td></tr>"
+            + "</table>"
+            ;
+
+  $("#globalOptions").append(globalOptionsInput);
+
+  if(domain != null){
+    //Input form for types
+    if(domain[0].length>0){
+      //Option Headings
+      var typeOptionsInput
+          = "<table><tr>"
+          + "<th>Type</th>"
+          + "<th>Default Image URL</th>"
+          + "<th>Visible?</th>"
+          + "</tr>";
+
+      var typeOptions
+          = "<td><textarea name=\"imageURL\" rows=\"1\" cols=\"25\"></textarea></td>"
+          + "<td><input name=\"visible\"type=\"checkbox\" checked></td>"
+          ;
+
+      for(var i=0; i<domain[0].length; i++){
+          typeOptionsInput += "<tr id=\"type_"+i+"\"><td>" + domain[0][i] + "</td>";
+          typeOptionsInput += typeOptions;
+          typeOptionsInput += "</tr>";
+      }
+      typeOptionsInput += "</table";
+      $("#typeOptions").append(typeOptionsInput);
+    }
+
+    if(domain[2].length>0) {
+      for(var i = 0; i<domain[2].length; i++){
+        var objectInput = "<p> " + domain[2][i] + "</p>";
+        //Set input options here.
+        $("#typeOptions").append(objectInput);
+      }
+    }
+  }
+}
+
+/*Gets the values from the HTML form and attaches them to the relevant
+objects*/
+function getInputValues() {
+//When adding something here, don't forget to add an equivalent entry in
+//setInputValues()
+
+  //globalOptions
+  globalOptions.spatialLayout = $("#globalOptions").find("select[name=spatialLayout]").val();
+  console.log(globalOptions);
+
+  if(domain!=null){
+  }
+  //typeOptions
+
+}
+
+/*
+****************      SAVE FORM INPUT        **********************
+*/
+
+
+/*
+****************      LOAD FORM INPUT        **********************
+*/
+
+
+/*
+****************      POPULATE OBJECTS       **********************
+*/
+
+function populateAnimationObjects(domain,problem,plan){
+
+  globalOptions = new GlobalOptions();
+
+
+}
 /*Create interface for setting up the animation
 Each object, constant and predicate should be listed with the following options
 General Options:
 - spatial layout (grid, network, free)
 */
+
+function TypeOptions(typeName, visible, defaultImageURL) {
+  this.typeName=typeName;
+  this.visible=visible;
+  this.defaultImageURL=defaultImageURL;
+}
 
 function GlobalOptions(spatialLayout) {
     this.spatialLayout = spatialLayout;
@@ -180,7 +257,7 @@ when the predicate evaluates to true and the second when it's false.
 */
 function animatedArgument(argument, type, setImage, setTransitionImage,
   setAnimation, setTranlation, setZ, setRelativePosition){
-
+    this.argument = argument;
 }
 
 function relativePosition(object, relativePosition) {
@@ -190,31 +267,6 @@ function relativePosition(object, relativePosition) {
 /*
 These options should be exportable via JSON.
 */
-
-
-function generateInputForm(domain) {
-  if(domain != null){
-
-    // typeOptions.appendChild("<p>Type Options</p>");
-    var typeInput = "<table>  <tr>  <th>Type</th> <th>Default Image URL</th>  </tr>";
-    for(var i=0; i<domain[0].length; i++){
-        var typeInput = "<tr><td>" + domain[0][i] + "</td>";
-        typeInput += "<td><textarea name=\"Image URL\" rows=\"256\" cols=\"1\"></textarea></td>";
-        typeInput += "</tr>";
-        //Set input options here.
-    }
-
-    typeInput += "</table";
-    console.log(typeInput);
-    $("#typeOptions").append(typeInput);
-
-    for(var i = 0; i<domain[2].length; i++){
-      var objectInput = "<p> " + domain[2][i] + "</p>";
-      //Set input options here.
-      $("#typeOptions").append(objectInput);
-    }
-  }
-}
 
 /*********************************************
             Pixi.js playground
